18b7fc524780a5f955b909c05836b4c69732b003
NGO <thanh.ngo@etu.univ-amu.fr>
2023-02-14 17:02:50 +0100

    Gestion des favoris ok

--- src/main/java/app/lyricsapp/FavoriteManager.java
+++ src/main/java/app/lyricsapp/FavoriteManager.java
@@ -2,33 +2,46 @@ package app.lyricsapp;
 
 import app.lyricsapp.model.ReadXMLFile;
 import app.lyricsapp.model.Song;
-
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class FavoriteManager {
-    private List<Song> favoriteSong;
+    private List<Song> favoriteSongs;
     File favoriteFile = new File("favoriteFile.xml");
     public FavoriteManager() {
-        favoriteSong = ReadXMLFile.readFile(favoriteFile);
+        favoriteSongs = ReadXMLFile.readFile(favoriteFile);
     }
 
-    public void writeFavoriteFile() {
-        //vider le fichier
-        for (Song song : favoriteSong) {
-            // ecrire song dans le fichier
+    public void writeFavoriteFile() throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(favoriteFile));
+        writer.write("<Favorite>\n");
+        for (Song song : favoriteSongs) {
+            writer.write(song.toXML());
         }
+        writer.write("</Favorite>\n");
+        writer.close();
     }
-    public void addFavorite(Song song) {
-        favoriteSong.add(song);
+    public void addFavorite(Song song) throws IOException {
+        favoriteSongs.add(song);
         writeFavoriteFile();
     }
-    public void removeFavorite(Song song) {
-        favoriteSong.remove(song);
+    public void removeFavorite(Song song) throws IOException {
+        favoriteSongs.remove(song);
         writeFavoriteFile();
     }
 
-    public File getFavoriteFile() {
-        return favoriteFile;
+    public List<Song> getFavoriteSongs() {
+        return favoriteSongs;
+    }
+
+    public static void main(String [] args) throws IOException {
+        File file = new File("query1.xml");
+        List<Song> songs = ReadXMLFile.readFile(file);
+
+        FavoriteManager favoriteManager = new FavoriteManager();
+        favoriteManager.addFavorite(songs.get(0));
     }
 }
--- src/main/java/app/lyricsapp/model/Song.java
+++ src/main/java/app/lyricsapp/model/Song.java
@@ -3,22 +3,30 @@ package app.lyricsapp.model;
 public class Song {
     private String lyricsCheckSum;
     private String lyricsId;
-    private String Artist;
-    private String Song;
+    private String artist;
+    private String song;
     public Song(String lyricsCheckSum, String lyricsId,String Artist,String Song){
         this.lyricsCheckSum=lyricsCheckSum;
         this.lyricsId=lyricsId;
-        this.Artist=Artist;
-        this.Song=Song;
+        this.artist =Artist;
+        this.song =Song;
     }
 
     @Override
     public String toString() {
-        return "Song{" +
-                "lyricsCheckSum='" + lyricsCheckSum + '\'' +
-                ", lyricsId='" + lyricsId + '\'' +
-                ", Artist='" + Artist + '\'' +
-                ", Song='" + Song + '\'' +
-                '}';
+        return "Song {" + "lyricsCheckSum='" + lyricsCheckSum + '\'' + ", lyricsId='" + lyricsId + '\'' + ", Artist='" + artist + '\'' + ", Song='" + song + '\'' + '}';
+    }
+
+    public String toXML() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("<SongFavorite>\n");
+        builder.append("\t<LyricsCheckSum>" + lyricsCheckSum + "</LyricsCheckSum>\n");
+        builder.append("\t<LyricsId>" + lyricsId + "</LyricsId>\n");
+        builder.append("\t<Artist>" + artist + "</Artist>\n");
+        builder.append("\t<Song>" + song + "</Song>\n");
+        builder.append("</SongFavorite>\n");
+
+        return builder.toString();
     }
 }
