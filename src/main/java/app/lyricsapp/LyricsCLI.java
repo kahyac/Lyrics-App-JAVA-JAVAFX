56d5c7eacc05c33677544ca9cfdb302ad311de9d
ilybo <ilyas.bouchenkir@etu.univ-amu.fr>
2023-02-21 11:29:12 +0100

    CLI

--- src/main/java/app/lyricsapp/LyricsCLI.java
+++ src/main/java/app/lyricsapp/LyricsCLI.java
@@ -13,18 +13,19 @@ public class LyricsCLI {
     private static FavoriteManager favorites = new FavoriteManager();
 
     public static void display() {
-        System.out.println("1- Rechercher une chanson");
+        System.out.println("\n\n1- Rechercher une chanson");
         System.out.println("2- Accéder aux favoris");
         System.out.println("3- Quitter");
+        System.out.print("\nSaisir votre choix : ");
     }
 
     public static void lobbyChoice(int choice) throws IOException {
-        int searchChoice = -1;
-        int favoriteChoice = -1;
+        int searchChoice;
+        int favoriteChoice;
         int element = 1;
 
         if (choice == 1) {
-            System.out.println("\nRechercher une chanson :\n");
+            System.out.println("\n\nRechercher une chanson :\n");
             System.out.println("1- Par titre et artiste");
             System.out.println("2- Par paroles");
             System.out.println("3- Retour");
@@ -36,21 +37,22 @@ public class LyricsCLI {
             } catch (Exception e) {
                 searchChoice = -1;
                 System.out.println("\nEntrée invalide");
+                display();
             }
             searchChoice(searchChoice);
         }
 
         else if (choice == 2) {
+            System.out.println("\n\nAccès aux favoris :\n");
             if(favorites.favoriteSongSize() == 0) {
-                System.out.println("\nVous n'avez pour l'instant pas encore ajouté de chansons parmi les favoris\n");
+                System.out.println("Vous n'avez pour l'instant pas encore ajouté de chansons parmi les favoris\n");
             } else {
-                System.out.println("\nVoici les chansons favorites :\n");
+                System.out.println("Voici les chansons favorites :\n");
                 for (Song favorite : favorites.getFavoriteSongs()) {
                     System.out.println(element + " " + favorite + "\n");
                     element++;
                 }
             }
-            System.out.println("Accès aux favoris :\n");
             System.out.println("1- Afficher les paroles d’une chanson parmi les favoris");
             System.out.println("2- Supprimer une chanson des favoris");
             System.out.println("3- Retour");
@@ -63,13 +65,10 @@ public class LyricsCLI {
             } catch (Exception e) {
                 favoriteChoice = -1;
                 System.out.println("\nEntrée invalide");
+                display();
             }
             favoriteChoice(favoriteChoice);
         }
-
-        /*else {
-            System.out.println("\nEntrée invalide");
-        }*/
     }
 
     public static void searchChoice(int searchChoice) throws IOException {
@@ -88,7 +87,6 @@ public class LyricsCLI {
 
             afterSearch();
         }
-
         else if (searchChoice == 2) {
 
             System.out.print("\nVeuillez inscrire les paroles de la chanson : ");
@@ -101,9 +99,8 @@ public class LyricsCLI {
 
             afterSearch();
         }
-
-        else {
-            System.out.println("\nEntrée invalide");
+        else if(searchChoice == 3) {
+            display();
         }
     }
 
@@ -113,18 +110,21 @@ public class LyricsCLI {
 
         System.out.println("\n1- Afficher les paroles de la chanson");
         System.out.println("2- Ajouter/retirer la chanson aux favoris");
+        System.out.println("3- Retour");
         System.out.print("\nSaisir votre choix : ");
 
         Scanner scanner = new Scanner(System.in);
         try {
             afterSearchChoice = scanner.nextInt();
         } catch (Exception e) {
-            afterSearchChoice = -1;
+            //afterSearchChoice = -1;
             System.out.println("\nEntrée invalide");
+            display();
         }
 
         if (afterSearchChoice == 1) {
             System.out.println("\nLyrics : " + Parse.getLyrics()[0]);
+            display();
         }
         else if (afterSearchChoice == 2) {
             if (favorites.songIsInFavoriteSongs(song)) {
@@ -136,53 +136,58 @@ public class LyricsCLI {
                 System.out.println("\nLa chanson a été ajouté aux favoris");
             }
         }
+        else if (afterSearchChoice == 3) {
+            display();
+        }
     }
 
     public static void favoriteChoice(int favoriteChoice) throws IOException {
-        int showSong = -1;
-        int deleteSong = -1;
-        if(favorites.favoriteSongSize() != 0) {
-            if (favoriteChoice == 1) {
-                System.out.print("Sélectionner une chanson afin d'afficher ses paroles : ");
-                Scanner scanner = new Scanner(System.in);
-                try {
-                    showSong = scanner.nextInt();
-                    System.out.println("\nVous avez sélectionné " + favorites.getSong(showSong));
-                    System.out.println(favorites.getSong(showSong).getLyrics());
-                } catch (Exception e) {
-                    showSong = -1;
-                    System.out.println("\nEntrée invalide");
+        int indexSong;
+        int deleteSong;
+
+        if(favoriteChoice == 3) {
+            display();
+        }
+        else if (favoriteChoice == 1 || favoriteChoice == 2) {
+            if(favorites.favoriteSongSize() != 0) {
+                if (favoriteChoice == 1) {
+                    System.out.print("Sélectionner une chanson afin d'afficher ses paroles : ");
+                    Scanner scanner = new Scanner(System.in);
+                    try {
+                        indexSong = scanner.nextInt();
+                        System.out.println("\nVous avez sélectionné " + favorites.getSong(indexSong));
+                        System.out.println("\n" + favorites.getSong(indexSong).getLyrics());
+                    } catch (Exception e) {
+                        //indexSong = -1;
+                        System.out.println("\nEntrée invalide");
+                        display();
+                    }
                 }
-            }
-            else if (favoriteChoice == 2) {
-                System.out.print("Sélectionner une chanson à supprimer des favoris : ");
-                Scanner scanner = new Scanner(System.in);
-                try {
-                    deleteSong = scanner.nextInt();
-                    System.out.println("\nVous avez décidé de supprimer des favoris " + favorites.getSong(deleteSong));
-                    favorites.removeFavorite(favorites.getSong(deleteSong));
-                } catch (Exception e) {
-                    deleteSong = -1;
-                    System.out.println("\nEntrée invalide");
+                else {
+                    System.out.print("Sélectionner une chanson à supprimer des favoris : ");
+                    Scanner scanner = new Scanner(System.in);
+                    try {
+                        deleteSong = scanner.nextInt();
+                        System.out.println("\nVous avez décidé de supprimer des favoris " + favorites.getSong(deleteSong));
+                        favorites.removeFavorite(favorites.getSong(deleteSong));
+                    } catch (Exception e) {
+                        //deleteSong = -1;
+                        System.out.println("\nEntrée invalide");
+                        display();
+                    }
                 }
-            }
-            else if (favoriteChoice == 3) {
-                //
             }
             else {
-                System.out.println("\nEntrée invalide");
+                System.out.println("\nDésolé, mais il est impossible d'effectuer cette action, car il n'y a aucune " +
+                        "chanson enregistrée dans les favoris");
+                display();
             }
-        }
-        else {
-            System.out.println("Désolé, mais il est impossible d'effectuer cette action, car il n'y a aucune " +
-                    "chanson enregistrée dans les favoris");
-            //retour en arrière
         }
     }
 
     public static void runCLI() throws IOException {
 
-        System.out.println("\nBienvenue sur LyricsApp\n");
+        System.out.println("\nBienvenue sur LyricsCLI");
 
         int choice = -1;
 
@@ -190,7 +195,6 @@ public class LyricsCLI {
             if(choice != 1 && choice != 2) {
                 display();
             }
-            System.out.print("\nSaisir votre choix : ");
             Scanner scanner = new Scanner(System.in);
 
             try {
@@ -203,7 +207,7 @@ public class LyricsCLI {
             lobbyChoice(choice);
         }
 
-        System.out.println("Fin du programme");
+        System.out.println("\nFin du programme");
     }
 
     public static void main(String[] args) throws IOException {
@@ -211,3 +215,4 @@ public class LyricsCLI {
     }
 
 }
+
