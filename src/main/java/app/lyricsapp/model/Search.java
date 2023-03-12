26308522da3d062ed385a418152ed4d3182dcc8c
ilybo <ilyas.bouchenkir@etu.univ-amu.fr>
2023-03-12 12:30:30 +0100

    GUI (suppression)

--- src/main/java/app/lyricsapp/model/Search.java
+++ src/main/java/app/lyricsapp/model/Search.java
@@ -9,7 +9,6 @@ public class Search {
         String url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist=" + artist + "&song=" + title;
         String response = API.call(url);
         Parse.idsChecksumsArtistsSongs(String.valueOf(response), false);
-        System.out.println(response);
     }
 
     public static void songByLyrics(String lyrics) {
